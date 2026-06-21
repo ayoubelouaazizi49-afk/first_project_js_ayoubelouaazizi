b2a77d668e174613ab7333c4b5528526c800fc19
ayoubelouaazizi49-afk <ayoubelouaazizi49@gmail.com>
2026-06-21 20:25:34 +0100

    feat: implement signUp input validation for  email

--- public/first-project.js
+++ public/first-project.js
@@ -1,5 +1,5 @@
-  let currentUser = null;
-  let user = [];
+let currentUser = null;
+let user = [];
 while (true) {
     let choice = prompt("1- Sign Up \n 2- Login \n 3- Change Password")
 
@@ -7,30 +7,43 @@ while (true) {
     if (choice === "1") {
         signUp();
 
-    }else if (choice === "2") {
+    } else if (choice === "2") {
         login()
 
-    }else if (choice === "3") {
-       changePassword()
+    } else if (choice === "3") {
+        changePassword()
 
     }
-     else if (choice === "exit") {
+    else if (choice === "exit") {
         break;
     }
 }
 // *signup
 function signUp() {
-    // !========Name=============================
+    // !name
     let nom = prompt("Enter Full Name").trim();
     if (nom.length < 5) {
         alert("Short name")
         return;
     }
-     nom = nom.split(' ')
-         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-         .join(' ');
+    nom = nom.split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+
+    // !email
+    let email = prompt("Enter Email").trim();
+    email = email.toLowerCase();
+    if (email.includes(" ")) {
+        alert("Remove Space");
+        return;
+    }
+    if (email.length < 10) {
+        alert("Email short")
+        return;
+    }
+    if (!email.includes("@")) {
+        alert("Email must contain @")
+        return;
+    }
 
 
-
-
-         
